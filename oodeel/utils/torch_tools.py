b871103fab321ec1ea6ec487a5566ab3418ea781
Yannick Prudent <yannick.prudent@irt-saintexupery.com>
2023-02-14 19:17:05 +0100

    feat: add torch grad wrt input

--- oodeel/utils/torch_tools.py
+++ oodeel/utils/torch_tools.py
@@ -20,3 +20,49 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import torch
+
+from typing import Callable
+
+
+def gradient_single(
+    model: Callable, inputs: torch.Tensor, targets: torch.Tensor
+) -> torch.Tensor:
+    """
+    Compute gradients for a batch of samples.
+    Parameters
+    ----------
+    model
+        Model used for computing gradient.
+    inputs
+        Input samples to be explained.
+    targets
+        One-hot encoded labels or regression target (e.g {+1, -1}), one for each sample.
+    Returns
+    -------
+    gradients
+        Gradients computed, with the same shape as the inputs.
+    """
+    inputs.requires_grad_(True)
+    score = torch.sum(model(inputs) * targets)
+    gradients = torch.autograd.grad(score, inputs)
+    inputs.requires_grad_(False)
+    return gradients[0]
+
+
+if __name__ == '__main__':
+    import torch.nn as nn
+    import torch.nn.functional as F
+
+    inputs = torch.randn(16, 3, 32, 32)  # bs, c, h, w
+    targets = F.one_hot(torch.randint(10, (16,)))
+    model = nn.Sequential(
+        nn.Conv2d(3, 32, 3, 1, 1),
+        nn.ReLU(),
+        nn.Conv2d(32, 16, 3, 1, 1),
+        nn.ReLU(),
+        nn.Flatten(),
+        nn.Linear(32*32*16, 10)
+    )
+    gradients = gradient_single(model, inputs, targets)
+    print(gradients.shape, inputs.requires_grad)
