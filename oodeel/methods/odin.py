fix: no need to softmax to get class predictions
Predicting classes can be done by simply taking argmax of the logits.
There is no need to compute softmax.

oodeel/methods/odin.py
@@ -71,7 +71,7 @@ class ODIN(OODModel):
     @tf.function
     def _input_perturbation(self, x):
         preds = self.feature_extractor.model(x)
-        preds = self.op.softmax(preds / self.temperature)
+        outputs_b = self.op.softmax(preds / self.temperature)
         num_classes = preds.shape[-1]
         outputs_b = self.op.one_hot(self.op.argmax(preds, axis=1), num_classes)
         gradients = self.op.gradient_model(self.feature_extractor.model, x, outputs_b)