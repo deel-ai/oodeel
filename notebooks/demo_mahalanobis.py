ab1122cd9caca7a26df69abcfda1ab726339f5d3
mlegoff <matthieu.le-goff@airbus.com>
2023-03-02 18:30:08 +0100

    feat: implement mahalanobis

---
+++ notebooks/demo_mahalanobis.py
@@ -0,0 +1,86 @@
+import os
+import pprint
+import warnings
+from dataclasses import dataclass
+from typing import Iterator
+
+import numpy as np
+import tensorflow as tf
+import torch
+from sklearn.metrics import accuracy_score, roc_auc_score
+from torch.utils.data import IterableDataset, DataLoader
+
+from oodeel.datasets import OODDataset
+from oodeel.eval.metrics import bench_metrics
+from oodeel.methods import DKNN
+from oodeel.methods.mahalanobis import Mahalanobis
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+warnings.filterwarnings("ignore")
+
+pp = pprint.PrettyPrinter()
+
+
+@dataclass
+class TFDSPytorchConverter(IterableDataset):
+    tf_ds: tf.data.Dataset
+
+    def __iter__(self) -> Iterator:
+        for batch_x, batch_y in self.tf_ds.as_numpy_iterator():
+            for sample_idx in range(batch_x.shape[0]):
+                yield np.stack([batch_x[sample_idx, :, :, 0] for _ in range(3)], axis=0), batch_y[sample_idx]
+
+
+if __name__ == '__main__':
+    oods_in = OODDataset('mnist', split="test")
+    oods_out = OODDataset('fashion_mnist', split="test")
+    oods_fit = OODDataset('mnist', split="train")
+
+
+    def preprocess_fn(*inputs):
+        x = inputs[0] / 255
+        return tuple([x] + list(inputs[1:]))
+
+
+    batch_size = 8
+    ds_in = oods_in.prepare(batch_size=batch_size, preprocess_fn=preprocess_fn)
+    ds_out = oods_out.prepare(batch_size=batch_size, preprocess_fn=preprocess_fn)
+    ds_fit = oods_fit.prepare(batch_size=batch_size, preprocess_fn=preprocess_fn, shuffle=True)
+
+    model = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar10_resnet20", pretrained=True)
+
+    ds_fit_pt = DataLoader(TFDSPytorchConverter(ds_fit.take(1_000)), batch_size=8)
+    ds_in_pt = DataLoader(TFDSPytorchConverter(ds_in.take(500)), batch_size=8)
+    ds_out_pt = DataLoader(TFDSPytorchConverter(ds_out.take(500)), batch_size=8)
+
+    ### DKNN
+
+    # oodmodel = DKNN(nearest=10, output_layers_id=["avgpool"])
+    # oodmodel.fit(model, ds_fit_pt)
+    # scores_in = oodmodel.score(ds_in_pt)
+    # scores_out = oodmodel.score(ds_out_pt)
+    #
+    # metrics = bench_metrics(
+    #     (scores_in, scores_out),
+    #     metrics=["auroc", "fpr95tpr", accuracy_score, roc_auc_score],
+    #     threshold=None
+    # )
+    #
+    # pp.pprint(metrics)
+
+    ### Mahalanobis
+
+    for mag in [0.0, 0.01, 0.005, 0.002, 0.0014, 0.001, 0.0005]:
+        oodmodel = Mahalanobis(output_layers_id=["avgpool"], input_processing_magnitude=mag)
+        oodmodel.fit(model, ds_fit_pt)
+        scores_in = oodmodel.score(ds_in_pt)
+        scores_out = oodmodel.score(ds_out_pt)
+
+        metrics = bench_metrics(
+            (scores_in, scores_out),
+            metrics=["auroc", "fpr95tpr", accuracy_score, roc_auc_score],
+            threshold=None
+        )
+
+        pp.pprint(metrics)
---
+++ oodeel/methods/mahalanobis.py
@@ -0,0 +1,172 @@
+from typing import List, Dict
+
+import torch
+from numpy.typing import ArrayLike
+from sklearn import preprocessing, covariance
+from torch.autograd import Variable
+
+from oodeel.methods.base import OODModel
+import numpy as np
+
+# TODO replace StandardScaler by computing the mean manually
+# TODO Simplify and document input processing code
+# TODO add the possibility to deactivate the input processing
+
+class Mahalanobis(OODModel):
+    """
+    Implementation from https://github.com/pokaxpoka/deep_Mahalanobis_detector
+    """
+
+    def __init__(self, input_processing_magnitude: float = 0.01, output_layers_id: List[int] = [-2], ):
+        super(Mahalanobis, self).__init__(output_layers_id=output_layers_id)
+
+        self.input_processing_magnitude = input_processing_magnitude
+        self.mean_covariance: covariance.EmpiricalCovariance = covariance.EmpiricalCovariance(assume_centered=True)
+        self.by_label_preprocessing: Dict[int, preprocessing.StandardScaler] = dict()
+        pass
+
+    def ___score_tensor(self, inputs: ArrayLike):
+
+        features = self.feature_extractor.predict(inputs).numpy()
+
+        if features.ndim > 2:
+            features = features.reshape(features.shape[0], -1)
+
+        return np.min(np.stack(
+            [self.mean_covariance.mahalanobis(self.by_label_preprocessing[lbl].transform(features)) for lbl in
+             self.by_label_preprocessing.keys()],
+            axis=0), axis=0)
+
+    def _score_tensor(self, inputs: ArrayLike):
+
+        """
+        Code was taken from https://github.com/pokaxpoka/deep_Mahalanobis_detector with minimal changes
+        """
+
+        magnitude = self.input_processing_magnitude
+
+        # Make sure that gradient will be retained
+        data = Variable(inputs, requires_grad=True)
+        out_features = self.feature_extractor.predict(data, detach=False)
+        # Flatten the features to 2D (n_batch, n_features)
+        out_features = out_features.view(out_features.size(0), -1)
+        # compute Mahalanobis score
+        # for each class, remove the mean and compute Mahalanobis distance
+        gaussian_score = 0
+        for i, lbl in enumerate(self.by_label_preprocessing.keys()):
+            # previously : batch_sample_mean = sample_mean[layer_index][i]
+            batch_sample_mean = torch.from_numpy(self.by_label_preprocessing[lbl].mean_)
+            #
+            zero_f = out_features.data - batch_sample_mean
+            # _precision = precision[layer_index]
+            _precision = torch.from_numpy(self.mean_covariance.covariance_).double()
+            term_gau = -0.5 * torch.mm(torch.mm(zero_f, _precision), zero_f.t()).diag()
+            if i == 0:
+                gaussian_score = term_gau.view(-1, 1)
+            else:
+                gaussian_score = torch.cat((gaussian_score, term_gau.view(-1, 1)), 1)
+
+        # Input_processing
+        sample_pred = gaussian_score.max(1)[1]
+        # previously: batch_sample_mean = sample_mean[layer_index].index_select(0, sample_pred)
+        batch_sample_mean = torch.from_numpy(
+            np.stack(
+                [self.by_label_preprocessing[list(self.by_label_preprocessing.keys())[s]].mean_ for s in sample_pred],
+                axis=0)
+        )
+        #
+        zero_f = out_features - Variable(batch_sample_mean)
+        # previously: _precision = precision[layer_index]
+        _precision = torch.from_numpy(self.mean_covariance.covariance_).double()
+        #
+        pure_gau = -0.5 * torch.mm(torch.mm(zero_f, Variable(_precision)), zero_f.t()).diag()
+        loss = torch.mean(-pure_gau)
+        loss.backward(retain_graph=True)
+
+        gradient = torch.ge(data.grad.data, 0)
+        gradient = (gradient.float() - 0.5) * 2
+        # TODO find out what the following lines are doing
+        # if net_type == 'densenet':
+        #     gradient.index_copy_(1, torch.LongTensor([0]).cuda(),
+        #                          gradient.index_select(1, torch.LongTensor([0]).cuda()) / (63.0 / 255.0))
+        #     gradient.index_copy_(1, torch.LongTensor([1]).cuda(),
+        #                          gradient.index_select(1, torch.LongTensor([1]).cuda()) / (62.1 / 255.0))
+        #     gradient.index_copy_(1, torch.LongTensor([2]).cuda(),
+        #                          gradient.index_select(1, torch.LongTensor([2]).cuda()) / (66.7 / 255.0))
+        # elif net_type == 'resnet':
+        #     gradient.index_copy_(1, torch.LongTensor([0]).cuda(),
+        #                          gradient.index_select(1, torch.LongTensor([0]).cuda()) / (0.2023))
+        #     gradient.index_copy_(1, torch.LongTensor([1]).cuda(),
+        #                          gradient.index_select(1, torch.LongTensor([1]).cuda()) / (0.1994))
+        #     gradient.index_copy_(1, torch.LongTensor([2]).cuda(),
+        #                          gradient.index_select(1, torch.LongTensor([2]).cuda()) / (0.2010))
+        tempInputs = torch.add(data.data, -magnitude, gradient)
+
+        noise_out_features = self.feature_extractor.predict(tempInputs)
+        noise_out_features = noise_out_features.view(noise_out_features.size(0), noise_out_features.size(1), -1)
+        noise_out_features = torch.mean(noise_out_features, 2)
+        noise_gaussian_score = 0
+        # previously
+        # for i in range(num_classes):
+        #     batch_sample_mean = sample_mean[layer_index][i]
+        #
+        for i, lbl in enumerate(self.by_label_preprocessing.keys()):
+            # previously, batch_sample_mean = sample_mean[layer_index][i]
+            batch_sample_mean = torch.from_numpy(self.by_label_preprocessing[lbl].mean_).double()
+            #
+            zero_f = noise_out_features.data - batch_sample_mean
+            # previously: _precision = precision[layer_index]
+            _precision = torch.from_numpy(self.mean_covariance.covariance_).double()
+            #
+            term_gau = -0.5 * torch.mm(torch.mm(zero_f, _precision), zero_f.t()).diag()
+            if i == 0:
+                noise_gaussian_score = term_gau.view(-1, 1)
+            else:
+                noise_gaussian_score = torch.cat((noise_gaussian_score, term_gau.view(-1, 1)), 1)
+
+        noise_gaussian_score, _ = torch.max(noise_gaussian_score, dim=1)
+
+        return noise_gaussian_score
+
+    def _fit_to_dataset(self, fit_dataset: ArrayLike):
+        # Store feature sets by label
+        features_by_label: Dict[int, List[np.ndarray]] = dict()
+
+        for batch in fit_dataset:
+            images, labels = batch
+            features = self.feature_extractor.predict(images)
+            for lbl in np.unique(labels.numpy()):
+                if not lbl in features_by_label.keys():
+                    features_by_label[lbl] = list()
+                _feat_np = features[labels == lbl, ::].numpy()
+                _feat_np = _feat_np.reshape(_feat_np.shape[0], -1)
+                features_by_label[lbl].append(
+                    _feat_np
+                )
+        for lbl in features_by_label.keys():
+            features_by_label[lbl] = np.vstack(features_by_label[lbl])
+
+        # Remove mean for each label and compute covariance
+        by_label_preprocessing = dict()
+        by_label_covariance = dict()
+        for lbl in features_by_label.keys():
+            ss = preprocessing.StandardScaler(with_mean=True, with_std=False)
+            ss.fit(features_by_label[lbl])
+
+            ec = covariance.EmpiricalCovariance(assume_centered=True)
+            ec.fit(ss.transform(features_by_label[lbl]))
+
+            by_label_preprocessing[lbl] = ss
+            by_label_covariance[lbl] = ec
+
+        # Take the mean of the per class covariances
+        self.mean_covariance = covariance.EmpiricalCovariance(assume_centered=True)
+        self.mean_covariance._set_covariance(
+            np.mean(np.stack([by_label_covariance[lbl].covariance_ for lbl in by_label_covariance.keys()], axis=0),
+                    axis=0)
+        )
+        labels = list(features_by_label.keys())
+        self.mean_covariance.location_ = np.zeros(features_by_label[labels[0]].shape[1])
+
+        self.by_label_preprocessing = by_label_preprocessing
+        self.by_label_covariance = by_label_covariance
