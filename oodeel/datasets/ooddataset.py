fix: torch op gradient tuple output

oodeel/datasets/ooddataset.py
@@ -36,6 +36,7 @@ from .tf_data_handler import TFDataHandler
 
 # OODDataset = TypeVar("OODDataset", bound="OODDadaset")
 
+
 class OODDataset(object):
     """Class for managing loading and processing of datasets that are to be used for
     OOD detection. The class encapsulates a dataset like object augmented with OOD
@@ -149,12 +150,12 @@ class OODDataset(object):
 
     def add_out_data(
         self,
-        out_dataset: Union['OODDataset', tf.data.Dataset],
+        out_dataset: Union["OODDataset", tf.data.Dataset],
         in_value: int = 0,
         out_value: int = 1,
         resize: Optional[bool] = False,
         shape: Optional[Tuple[int]] = None,
-    ) -> 'OODDataset':
+    ) -> "OODDataset":
         """Concatenate two OODDatasets. Useful for scoring on multiple datasets, or
         training with added out-of-distribution data.
 
@@ -209,7 +210,7 @@ class OODDataset(object):
         self,
         in_labels: Optional[Union[np.ndarray, list]] = None,
         out_labels: Optional[Union[np.ndarray, list]] = None,
-    ) -> Optional[Tuple['OODDataset']]:
+    ) -> Optional[Tuple["OODDataset"]]:
         """Filter the dataset by assigning ood labels depending on labels
         value (typically, class id).
 

oodeel/methods/dknn.py
@@ -51,9 +51,9 @@ class DKNN(OODModel):
     """
 
     def __init__(
-            self,
-            nearest: int = 1,
-            output_layers_id: List[int] = [-2],
+        self,
+        nearest: int = 1,
+        output_layers_id: List[int] = [-2],
     ):
         super().__init__(
             output_layers_id=output_layers_id,
@@ -63,7 +63,7 @@ class DKNN(OODModel):
         self.nearest = nearest
 
     def _fit_to_dataset(
-            self, fit_dataset: Union[tf.data.Dataset, tf.Tensor, np.ndarray]
+        self, fit_dataset: Union[tf.data.Dataset, tf.Tensor, np.ndarray]
     ):
         """
         Constructs the index from ID data "fit_dataset", which will be used for
@@ -80,7 +80,7 @@ class DKNN(OODModel):
         self.index.add(norm_fit_projected)
 
     def _score_tensor(
-            self, inputs: Union[tf.data.Dataset, tf.Tensor, np.ndarray]
+        self, inputs: Union[tf.data.Dataset, tf.Tensor, np.ndarray]
     ) -> np.ndarray:
         """
         Computes an OOD score for input samples "inputs" based on

oodeel/methods/mahalanobis.py
@@ -1,9 +1,34 @@
-from enum import IntEnum, auto
-from typing import List, Dict
+# -*- coding: utf-8 -*-
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+from enum import auto
+from enum import IntEnum
+from typing import Dict
+from typing import List
 
 import numpy as np
 from numpy.typing import ArrayLike
-from sklearn import preprocessing, covariance
+from sklearn import covariance
+from sklearn import preprocessing
 
 from oodeel.methods.base import OODModel
 
@@ -18,15 +43,18 @@ class Mahalanobis(OODModel):
     Implementation from https://github.com/pokaxpoka/deep_Mahalanobis_detector
     """
 
-    def __init__(self,
-                 input_processing_magnitude: float = 0.0,
-                 output_layers_id: List[int] = [-2],
-                 mode: str = "default"
-                 ):
+    def __init__(
+        self,
+        input_processing_magnitude: float = 0.0,
+        output_layers_id: List[int] = [-2],
+        mode: str = "default",
+    ):
         super(Mahalanobis, self).__init__(output_layers_id=output_layers_id)
 
         self.input_processing_magnitude = input_processing_magnitude
-        self.mean_covariance: covariance.EmpiricalCovariance = covariance.EmpiricalCovariance(assume_centered=True)
+        self.mean_covariance: covariance.EmpiricalCovariance = (
+            covariance.EmpiricalCovariance(assume_centered=True)
+        )
         self.by_label_preprocessing: Dict[int, preprocessing.StandardScaler] = dict()
         self.mode: MahalanobisMode = MahalanobisMode[mode]
 
@@ -45,10 +73,18 @@ class Mahalanobis(OODModel):
         if features.ndim > 2:
             features = features.reshape(features.shape[0], -1)
 
-        return np.min(np.stack(
-            [self.mean_covariance.mahalanobis(self.by_label_preprocessing[lbl].transform(features)) for lbl in
-             self.by_label_preprocessing.keys()],
-            axis=0), axis=0)
+        return np.min(
+            np.stack(
+                [
+                    self.mean_covariance.mahalanobis(
+                        self.by_label_preprocessing[lbl].transform(features)
+                    )
+                    for lbl in self.by_label_preprocessing.keys()
+                ],
+                axis=0,
+            ),
+            axis=0,
+        )
 
     def _score_tensor_default(self, inputs: ArrayLike):
 
@@ -66,8 +102,12 @@ class Mahalanobis(OODModel):
         sample_pred = gaussian_score.max(1)[1]
 
         means = self.op.stack(
-            [self.get_mean_by_label(list(self.by_label_preprocessing.keys())[s]) for s in sample_pred],
-            dim=0)
+            [
+                self.get_mean_by_label(list(self.by_label_preprocessing.keys())[s])
+                for s in sample_pred
+            ],
+            dim=0,
+        )
 
         gradient = self.op.gradient(self.loss_fn, inputs, means=means)
         gradient = self.op.sign(gradient)
@@ -90,7 +130,10 @@ class Mahalanobis(OODModel):
             batch_sample_mean = self.get_mean_by_label(lbl)
             zero_f = out_features - batch_sample_mean
             term_gau = -0.5 * self.op.diag(
-                self.op.matmul(self.op.matmul(zero_f, _precision), self.op.transpose(zero_f)))
+                self.op.matmul(
+                    self.op.matmul(zero_f, _precision), self.op.transpose(zero_f)
+                )
+            )
             gaussian_scores.append(self.op.reshape(term_gau, (-1, 1)))
         gaussian_score = self.op.cat(gaussian_scores, 1)
         return gaussian_score
@@ -106,8 +149,12 @@ class Mahalanobis(OODModel):
         # Flatten the features to 2D (n_batch, n_features)
         _out_features = self.op.flatten(_out_features)
         _zero_f = _out_features - self.op.constant(means)
-        pure_gau = -0.5 * self.op.diag(self.op.matmul(self.op.matmul(_zero_f, self.op.constant(self.get_precision())),
-                                                      self.op.transpose(_zero_f)))
+        pure_gau = -0.5 * self.op.diag(
+            self.op.matmul(
+                self.op.matmul(_zero_f, self.op.constant(self.get_precision())),
+                self.op.transpose(_zero_f),
+            )
+        )
         return self.op.mean(-pure_gau)
 
     def _fit_to_dataset(self, fit_dataset: ArrayLike):
@@ -122,9 +169,7 @@ class Mahalanobis(OODModel):
                     features_by_label[lbl] = list()
                 _feat_np = self.op.to_numpy(features[labels == lbl, ::])
                 _feat_np = _feat_np.reshape(_feat_np.shape[0], -1)
-                features_by_label[lbl].append(
-                    _feat_np
-                )
+                features_by_label[lbl].append(_feat_np)
         for lbl in features_by_label.keys():
             features_by_label[lbl] = np.vstack(features_by_label[lbl])
 
@@ -144,8 +189,16 @@ class Mahalanobis(OODModel):
         # Take the mean of the per class covariances
         self.mean_covariance = covariance.EmpiricalCovariance(assume_centered=True)
         self.mean_covariance._set_covariance(
-            np.mean(np.stack([by_label_covariance[lbl].covariance_ for lbl in by_label_covariance.keys()], axis=0),
-                    axis=0)
+            np.mean(
+                np.stack(
+                    [
+                        by_label_covariance[lbl].covariance_
+                        for lbl in by_label_covariance.keys()
+                    ],
+                    axis=0,
+                ),
+                axis=0,
+            )
         )
         labels = list(features_by_label.keys())
         self.mean_covariance.location_ = np.zeros(features_by_label[labels[0]].shape[1])

oodeel/methods/odin.py
@@ -46,7 +46,7 @@ class ODIN(OODModel):
 
     def __init__(self, temperature: float = 1000, noise: float = 0.014):
         self.temperature = temperature
-        super().__init__(output_layers_id=[-1], input_layers_id=0)
+        super().__init__(output_layers_id=[-1], input_layers_id=None)
         self.noise = noise
         self._loss_func = tf.keras.losses.SparseCategoricalCrossentropy(
             from_logits=True, reduction="sum"

oodeel/models/torch_feature_extractor.py
@@ -25,12 +25,10 @@ from typing import Union
 
 import torch
 from torch import nn
-from torch.utils.data import Dataset, IterableDataset
-from torch.utils.data.dataset import T_co
+from torch.utils.data import Dataset
 
 from ..utils.tf_utils import get_input_from_dataset_elem
 from .feature_extractor import FeatureExtractor
-import tensorflow as tf
 
 
 class TorchFeatureExtractor(FeatureExtractor):
@@ -59,10 +57,10 @@ class TorchFeatureExtractor(FeatureExtractor):
     """
 
     def __init__(
-            self,
-            model: nn.Module,
-            output_layers_id: List[Union[int, str]] = [],
-            input_layer_id: Union[int, str] = None,
+        self,
+        model: nn.Module,
+        output_layers_id: List[Union[int, str]] = [],
+        input_layer_id: Union[int, str] = None,
     ):
         super().__init__(
             model=model,
@@ -113,20 +111,20 @@ class TorchFeatureExtractor(FeatureExtractor):
 
     def prepare_extractor(self):
         """
-               Prepare the feature extractor for inference.
-               """
+        Prepare the feature extractor for inference.
+        """
         # Register a hook to store feature values for each considered layer.
         for layer_id in self.output_layers_id:
             layer = self.find_layer(layer_id)
             layer.register_forward_hook(self.get_features_hook(layer_id))
 
         # Crop model if input layer is provided
-        if (not (self.input_layer_id) is None):
+        if not (self.input_layer_id) is None:
             if isinstance(self.input_layer_id, int):
                 if self.input_layer_id > 0:
                     if isinstance(self.model, nn.Sequential):
                         self.model = nn.Sequential(
-                            *list(self.model.modules())[self.input_layer_id:]
+                            *list(self.model.modules())[self.input_layer_id :]
                         )
                     else:
                         raise NotImplementedError
@@ -140,7 +138,7 @@ class TorchFeatureExtractor(FeatureExtractor):
                 )
                 input_module_idx = module_names.index(self.input_layer_id)
                 self.model = nn.Sequential(
-                    *list(self.model.modules())[(input_module_idx + 1):]
+                    *list(self.model.modules())[(input_module_idx + 1) :]
                 )
 
             else:
@@ -155,9 +153,7 @@ class TorchFeatureExtractor(FeatureExtractor):
             x = x.to(self._device)
         _ = self.model(x)
 
-        features = [
-            self._features[layer_id] for layer_id in self.output_layers_id
-        ]
+        features = [self._features[layer_id] for layer_id in self.output_layers_id]
 
         if len(features) == 1:
             features = features[0]

oodeel/utils/torch_operator.py
@@ -32,7 +32,6 @@ from .operator import Operator
 
 
 class TorchOperator(Operator):
-
     @staticmethod
     def softmax(tensor: Union[torch.Tensor, np.ndarray]) -> torch.Tensor:
         """Softmax function"""
@@ -79,9 +78,8 @@ class TorchOperator(Operator):
             Gradients computed, with the same shape as the inputs.
         """
         inputs.requires_grad_(True)
-        # inputs = Variable(inputs, requires_grad=True)
         outputs = func(inputs, *args, **kwargs)
-        gradients = torch.autograd.grad(outputs, inputs)
+        gradients = torch.autograd.grad(outputs, [inputs])
         inputs.requires_grad_(False)
         return gradients[0]
 

tests/utils/test_torch_operator.py
@@ -34,7 +34,7 @@ def test_gradient():
 
     x = torch.ones(input_shape)
     torch_operator = TorchOperator()
-    gradients = torch_operator.gradient(diff_fun, x)[0]
+    gradients = torch_operator.gradient(diff_fun, x)
 
     assert tuple(gradients.shape) == input_shape
     assert torch.all(gradients == torch.ones(input_shape))