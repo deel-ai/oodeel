19c9ff8ba710e6e62f6dcca441d9cad8db714c8a
paulnovello <pnovellop@gmail.com>
2023-05-10 13:15:20 +0200

    docs: Final doc skeleton

--- docs/api/methods.md
+++ docs/api/methods.md
@@ -1,11 +1,8 @@
 ::: oodeel.methods.base
     options:
         show_root_toc_entry: True
-        inherited_members: True
-        filters: None
 
 ::: oodeel.methods
     options:
         show_root_toc_entry: True
-        inherited_members: True
-        filters: None
+        filters: []
--- oodeel/models/__init__.py
+++ oodeel/models/__init__.py
@@ -20,3 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+from .keras_feature_extractor import KerasFeatureExtractor
+from .torch_feature_extractor import TorchFeatureExtractor
+
+_all_ = ["KerasFeatureExtractor", "TorchFeatureExtractor"]
