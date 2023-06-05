0e68ebe4b4b74eda37b785e4ffe50fa178cfafcd
cofri <cofri@users.noreply.github.com>
2023-06-05 18:21:30 +0200

    test(metrics): tests for "detect_acc" and sklearn "roc_auc_score" metrics

--- tests/tests_tensorflow/eval/test_tf_metrics.py
+++ tests/tests_tensorflow/eval/test_tf_metrics.py
@@ -23,7 +23,7 @@
 import numpy as np
 import pytest
 from pytest import approx
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 from oodeel.eval.metrics import bench_metrics
 from oodeel.eval.metrics import ftpn
@@ -68,19 +68,30 @@ def test_get_curve():
 def test_bench_metrics(in_value, out_value):
     labels = np.array([in_value] * 5 + [out_value] * 5)  # 5 ID + 5 OOD samples
     scores = np.array([0.0] + [0.2] * 6 + [1.0] * 3)  # ID: 1x0, 4x0.2 / OOD: 2x0.1, 3x1
+    metrics = [
+        "auroc",
+        "tpr50fpr",
+        "fpr15tnr",
+        "tnr90fpr",
+        accuracy_score,
+        roc_auc_score,
+        "detect_acc",
+    ]
     # get metrics
     metrics_dict = bench_metrics(
         scores,
         labels,
         in_value,
         out_value,
-        metrics=["auroc", "tpr50fpr", "fpr15tnr", "tnr90fpr", accuracy_score],
+        metrics,
         threshold=0.5,
         step=1,
     )
     # expected results (hand calculated)
     assert metrics_dict["auroc"] == approx(1 - (0.4 * 0.8) / 2)
+    assert metrics_dict["roc_auc_score"] == approx(1 - (0.4 * 0.8) / 2)
     assert metrics_dict["accuracy_score"] == approx(8 / 10)
+    assert metrics_dict["detect_acc"] == approx(8 / 10)
     assert metrics_dict["tpr50fpr"] == 0.6
     assert metrics_dict["fpr15tnr"] == 0.8
     assert metrics_dict["tnr90fpr"] == 0.2
@@ -90,7 +101,7 @@ def test_bench_metrics(in_value, out_value):
     scores_out = scores[labels == out_value]
     metrics_dict_2 = bench_metrics(
         (scores_in, scores_out),
-        metrics=["auroc", "tpr50fpr", "fpr15tnr", "tnr90fpr", accuracy_score],
+        metrics=metrics,
         threshold=0.5,
         step=1,
     )
