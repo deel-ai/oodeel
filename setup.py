8363e52094440859080632f1e83c4b378e03557b
paul.novello <paul.novello@irt-saintexupery.com>
2023-09-15 15:26:36 +0200

    build: Bump version 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     # Name of the package:
     name="oodeel",
     # Version of the package:
-    version="0.1.1",
+    version="0.2.0",
     # Find the package automatically (include everything):
     packages=find_packages(),
     # Author information:
