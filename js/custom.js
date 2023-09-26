21b8181e38e78be55e31abcea1676ab5d518e1a0
Yannick Prudent <yannick.prudent@irt-saintexupery.com>
2023-09-26 17:05:42 +0200

    Deployed 79235fa with MkDocs version: 1.5.3

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,10 @@
-window.MathJax = {
-  tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
-    processEscapes: true,
-    processEnvironments: true,
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
-  },
-};
+document$.subscribe(({ body }) => {
+  renderMathInElement(body, {
+    delimiters: [
+      { left: "$$",  right: "$$",  display: true },
+      { left: "$",   right: "$",   display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true }
+    ],
+  })
+})
