704fda4a00902e17ee8a21c368f0234ab24cce2c
paulnovello <paul.novello@irt-saintexupery.com>
2024-04-18 15:58:53 +0200

    chore: flake8

--- tests/tests_tensorflow/methods/test_tf_ash.py
+++ tests/tests_tensorflow/methods/test_tf_ash.py
@@ -23,7 +23,6 @@
 import pytest
 
 from oodeel.methods import Energy
-from oodeel.methods import Entropy
 from oodeel.methods import MLS
 from oodeel.methods import ODIN
 from tests.tests_tensorflow import eval_detector_on_blobs
