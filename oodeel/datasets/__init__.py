bd4f66133217bbc83e3c8989c351a11bf9b1e421
paulnovello <paul.novello@irt-saintexupery.com>
2025-02-01 19:52:50 +0100

    feat: add data_handler_loader to laod the appropriate DataHandler depending on the backend

--- oodeel/datasets/__init__.py
+++ oodeel/datasets/__init__.py
@@ -20,4 +20,5 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+from .data_handler import data_handler_loader
 from .deprecated.ooddataset import OODDataset
