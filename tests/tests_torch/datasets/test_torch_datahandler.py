633513332f278f9cc688398603cd1db9c94670c9
paulnovello <paul.novello@irt-saintexupery.com>
2025-02-01 23:50:08 +0100

    test: tests for TorchDataHandler

--- tests/tests_torch/datasets/test_torch_datahandler.py
+++ tests/tests_torch/datasets/test_torch_datahandler.py
@@ -22,12 +22,76 @@
 # SOFTWARE.
 import tempfile
 
+import numpy as np
 import pytest
 import torch
+import torchvision
 
+from oodeel.datasets import data_handler_loader
+from oodeel.datasets.torch_data_handler import Dataset
+from oodeel.datasets.torch_data_handler import dict_only_ds
+from oodeel.datasets.torch_data_handler import DictDataset
 from oodeel.datasets.torch_data_handler import TorchDataHandler
 from tests.tests_torch import generate_data
 from tests.tests_torch import generate_data_torch
+
+
+def assign_feature_value(
+    dataset: DictDataset, feature_key: str, value: int
+) -> DictDataset:
+    """Assign a value to a feature for every sample in a DictDataset
+
+    Args:
+        dataset (DictDataset): DictDataset to assign the value to
+        feature_key (str): Feature to assign the value to
+        value (int): Value to assign
+
+    Returns:
+        DictDataset
+    """
+    assert isinstance(
+        dataset, DictDataset
+    ), "Dataset must be an instance of DictDataset"
+
+    def assign_value_to_feature(x):
+        x[feature_key] = torch.tensor(value)
+        return x
+
+    dataset = dataset.map(assign_value_to_feature)
+    return dataset
+
+
+def get_dataset_length(dataset: Dataset) -> int:
+    """Number of items in a dataset
+
+    Args:
+        dataset (DictDataset): Dataset
+
+    Returns:
+        int: Dataset length
+    """
+    return len(dataset)
+
+
+@dict_only_ds
+def get_feature_from_ds(dataset: DictDataset, feature_key: str) -> np.ndarray:
+    """Get a feature from a DictDataset
+
+    !!! note
+        This function can be a bit time consuming since it needs to iterate
+        over the whole dataset.
+
+    Args:
+        dataset (DictDataset): Dataset to get the feature from
+        feature_key (str): Feature value to get
+
+    Returns:
+        np.ndarray: Feature values for dataset
+    """
+
+    features = dataset.map(lambda x: x[feature_key])
+    features = np.stack([f.numpy() for f in features])
+    return features
 
 
 def test_get_item_length():
@@ -41,6 +105,11 @@ def test_get_item_length():
 
     length = TorchDataHandler.get_item_length(data)
     assert length == 2
+
+
+def test_instanciate_tf_datahandler():
+    handler = data_handler_loader(backend="torch")
+    assert isinstance(handler, TorchDataHandler)
 
 
 def test_get_feature_shape():
@@ -89,7 +158,7 @@ def test_get_input_from_dataset_item():
         ("MNIST", False),
     ],
 )
-def test_load_torchvision(dataset_name, train, erase_after_test=True):
+def test_instanciate_from_torchvision(dataset_name, train, erase_after_test=True):
     DATASET_INFOS = {
         "MNIST": {
             "img_shape": (1, 28, 28),
@@ -211,19 +280,19 @@ def test_data_handler_full_pipeline(x_shape, num_samples, num_labels, one_hot):
         item["new_feature"] = item["new_feature"] * 3 + 2
         return item
 
-    dataset_a = handler.assign_feature_value(dataset_a, "new_feature", 0)
+    dataset_a = assign_feature_value(dataset_a, "new_feature", 0)
     dataset_a = dataset_a.map(map_fn_a)
-    features_a = torch.Tensor(handler.get_feature_from_ds(dataset_a, "new_feature"))
+    features_a = torch.Tensor(get_feature_from_ds(dataset_a, "new_feature"))
     assert torch.all(features_a == torch.Tensor([-3] * num_samples_a))
 
-    dataset_b = handler.assign_feature_value(dataset_b, "new_feature", 1)
+    dataset_b = assign_feature_value(dataset_b, "new_feature", 1)
     dataset_b = dataset_b.map(map_fn_b)
-    features_b = torch.Tensor(handler.get_feature_from_ds(dataset_b, "new_feature"))
+    features_b = torch.Tensor(get_feature_from_ds(dataset_b, "new_feature"))
     assert torch.all(features_b == torch.Tensor([5] * num_samples_b))
 
     # concatenate two sub datasets
     dataset_c = handler.merge(dataset_a, dataset_b)
-    features_c = torch.Tensor(handler.get_feature_from_ds(dataset_c, "new_feature"))
+    features_c = torch.Tensor(get_feature_from_ds(dataset_c, "new_feature"))
     assert torch.all(features_c == torch.cat([features_a, features_b]))
 
     # prepare dataloader
@@ -234,3 +303,142 @@ def test_data_handler_full_pipeline(x_shape, num_samples, num_labels, one_hot):
         torch.Size([64, num_labels]) if one_hot else torch.Size([64])
     )
     assert batch[2].shape == torch.Size([64])
+
+
+@pytest.mark.parametrize(
+    "in_labels, out_labels, one_hot, expected_output",
+    [
+        ([1, 2], None, False, [100, 67, 33, 2, 1]),
+        (None, [1, 2], True, [100, 33, 67, 1, 2]),
+        ([1], [2], False, [100, 33, 34, 1, 1]),
+    ],
+    ids=[
+        "[torch] Assign OOD labels by class with ID labels",
+        "[torch] Assign OOD labels by class with OOD labels",
+        "[torch] Assign OOD labels by class with ID and OOD labels",
+    ],
+)
+def test_split_by_class(in_labels, out_labels, one_hot, expected_output):
+    """Test the split_by_class method."""
+
+    # generate data
+    x_shape = (3, 32, 32)
+    images, labels = generate_data(
+        x_shape=x_shape,
+        num_labels=3,
+        samples=100,
+        one_hot=one_hot,
+    )
+
+    if not one_hot:
+        for i in range(100):
+            if i < 33:
+                labels[i] = 0
+            if i >= 33 and i < 66:
+                labels[i] = 1
+            if i >= 66:
+                labels[i] = 2
+    else:
+        for i in range(100):
+            if i < 33:
+                labels[i] = np.array([1, 0, 0])
+            if i >= 33 and i < 66:
+                labels[i] = np.array([0, 1, 0])
+            if i >= 66:
+                labels[i] = np.array([0, 0, 1])
+
+    handler = TorchDataHandler()
+    dataset = handler.load_dataset(dataset_id=(images, labels))
+
+    in_dataset, out_dataset = handler.split_by_class(
+        dataset=dataset,
+        in_labels=in_labels,
+        out_labels=out_labels,
+    )
+
+    len_ds = len(dataset)
+    len_inds = len(in_dataset)
+    len_outds = len(out_dataset)
+
+    classes = get_feature_from_ds(dataset, "label")
+    classes = np.unique(classes, axis=0)
+
+    classes_in = get_feature_from_ds(in_dataset, "label")
+    classes_in = np.unique(classes_in, axis=0)
+
+    classes_out = get_feature_from_ds(out_dataset, "label")
+    classes_out = np.unique(classes_out, axis=0)
+
+    assert len_ds == expected_output[0]
+    assert len_inds == expected_output[1]
+    assert len_outds == expected_output[2]
+    assert len(classes_in) == expected_output[3]
+    assert len(classes_out) == expected_output[4]
+
+
+@pytest.mark.parametrize(
+    "shuffle, with_labels, expected_output",
+    [
+        (False, True, [2, (16, 10)]),
+        (False, False, [1, (16,)]),
+        (True, True, [2, (16, 10)]),
+        (True, False, [1, (16,)]),
+    ],
+    ids=[
+        "[torch] Prepare OODDataset for scoring with labels and ood labels",
+        "[torch] Prepare OODDataset for scoring with only ood labels",
+        "[torch] Prepare OODDataset for training (with shuffle and augment_fn) with "
+        "labels and ood labels",
+        "[torch] Prepare OODDataset for training (with shuffle and augment_fn) "
+        "with only labels",
+    ],
+)
+def test_prepare(shuffle, with_labels, expected_output):
+    """Test the prepare method."""
+
+    num_labels = 10
+    batch_size = 16
+    samples = 100
+
+    x_shape = (3, 32, 32)
+
+    handler = TorchDataHandler()
+    dataset = handler.load_dataset(
+        dataset_id=generate_data_torch(
+            x_shape=x_shape,
+            num_labels=num_labels,
+            samples=samples,
+            one_hot=True,
+        )
+    )
+
+    def preprocess_fn(*inputs):
+        x = inputs[0] / 255
+        return tuple([x] + list(inputs[1:]))
+
+    def augment_fn_(*inputs):
+        x = torchvision.transforms.RandomHorizontalFlip()(inputs[0])
+        return tuple([x] + list(inputs[1:]))
+
+    augment_fn = augment_fn_ if shuffle else None
+
+    ds = handler.prepare(
+        dataset,
+        batch_size=batch_size,
+        preprocess_fn=preprocess_fn,
+        with_labels=with_labels,
+        shuffle=shuffle,
+        augment_fn=augment_fn,
+    )
+
+    batch1 = next(iter(ds))
+    batch2 = next(iter(ds))
+
+    assert len(batch1) == expected_output[0]
+    if shuffle:
+        assert torch.sum(batch1[0] - batch2[0]) != 0
+    if with_labels:
+        assert batch1[1].shape == torch.Size([batch_size, num_labels])
+    assert batch1[0].shape == torch.Size([batch_size, 3, 32, 32])
+    assert torch.max(batch1[0]) <= 1
+    assert torch.min(batch1[0]) >= 0
