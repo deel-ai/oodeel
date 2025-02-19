4af7700a5980f10acc8721246841566671722a12
paulnovello <paul.novello@irt-saintexupery.com>
2025-02-19 10:59:59 +0100

    chore: flake8

--- oodeel/extractor/torch_feature_extractor.py
+++ oodeel/extractor/torch_feature_extractor.py
@@ -20,7 +20,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from collections import OrderedDict
 from typing import get_args
 from typing import Optional
 
